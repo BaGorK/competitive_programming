39c1132a69cebd174be672fb496dac1b29baeaca
Edmealem.k <edmealemkassahun@gmail.com>
2025-08-09 23:08:21 +0300

    Time: 0 ms (100.00%) | Memory: 55.4 MB (38.10%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,18 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const indices = {}; // val -> index
-
-    for (let i = 0; i < nums.length; i++) {
-        indices[nums[i]] = i;
+  const prevMark = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (prevMark.has(diff)) {
+        return [prevMark.get(diff), i] 
     }
-
-    for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i];
-        if (indices[diff] !== undefined && indices[diff] !== i) {
-            return [i, indices[diff]];
-        }
-    }
-
-    return [];
+    prevMark.set(nums[i], i)
+  }
+  return []
 };
