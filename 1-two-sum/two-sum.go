ccf3f4c3894caf4471dc03ab4a5b8d7c4d313a4c
Edmealem.k <edmealemkassahun@gmail.com>
2025-08-09 23:34:54 +0300

    Time: 0 ms (100.00%) | Memory: 6 MB (41.38%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,10 +1,11 @@
 func twoSum(nums []int, target int) []int {
-    for i := 0; i < len(nums); i++ {
-        for j := i + 1; j < len(nums); j++ {
-            if nums[i] + nums[j] == target {
-                return []int{i, j}
-            }
+    prevMap := make(map[int]int)
+    for i,n := range nums {
+        diff := target - n
+        if j, found := prevMap[diff]; found {
+            return []int{j,i}
         }
+        prevMap[n] = i
     }
     return []int{}
 }
