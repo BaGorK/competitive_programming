e8c1663d2eedefa2087b86f0c291af34bd9db77b
Edmealem.k <edmealemkassahun@gmail.com>
2025-08-14 10:13:25 +0300

    Time: 0 ms (100.00%) | Memory: 10.4 MB (41.05%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.go
+++ 238-product-of-array-except-self/product-of-array-except-self.go
@@ -1,18 +1,18 @@
 func productExceptSelf(nums []int) []int {
-    res := make([]int, len(nums))
-    for i := range res {
+    n := len(nums);
+    res := make([]int, n)
+    for i, _ := range res {
         res[i] = 1
     }
 
     prefix := 1
-    for i := 0; i < len(nums); i++ {
+    for i := 0; i < n; i++ {
         res[i] = prefix
         prefix *= nums[i]
     }
-
-    postfix := 1
+    postfix := 1;
     for i := len(nums) - 1; i >= 0; i-- {
-        res[i] *= postfix
+        res[i] *= postfix;
         postfix *= nums[i]
     }
 
